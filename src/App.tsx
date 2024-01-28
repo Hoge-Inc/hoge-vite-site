0fc7a00af93c7bb369e2be26e08f742b323f1a11
zembahk <77575523+zembahk@users.noreply.github.com>
2024-01-27 18:20:08 -0800

    vite-site-start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from '/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+  
+    <img src={logo} className="logo" alt="Hoge logo" />
+  
+  </>
+  )
+}
+
+export default App
